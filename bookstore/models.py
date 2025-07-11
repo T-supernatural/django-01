9a8c7ee2180bb9913288872e5da0acc063b0eb74
T-supernatural <tinuademichael@gmail.com>
2025-07-11 22:07:38 +0100

    next changes

--- bookstore/models.py
+++ bookstore/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField("Book Title", max_length=255)
+    description = models.TextField("Description")
+    price = models.PositiveIntegerField("Price (₦)", default=0)
+    add_date = models.DateTimeField("Date Added", auto_now=True)
+    cover = models.ImageField("Cover Image", upload_to="covers")
+
+    def __str__(self):
+        return self.title
+    
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Book
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'bookstore/home.html')
+    book = Book.objects.all
+    context = {
+        'book': book
+    }
+    return render(request, 'home.html', context)
